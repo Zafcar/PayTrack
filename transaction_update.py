19ec481e9b1c5452574b88434ef8ba5d71444bfb
Zafcar <50046161+Zafcar@users.noreply.github.com>
2022-07-13 16:02:41 +0530

    Appending transaction file is working
    Finally fixed the appending_excel method.
    Also remove the reading_excel as it is redundant method.
    Also removed useless libraries.

--- transaction_update.py
+++ transaction_update.py
@@ -1,7 +1,6 @@
 # Libraries
 import pandas as pd
 import os
-import streamlit as st
 import datetime
 
 # Selecting which execl to read.
@@ -9,8 +8,7 @@ path = "C:/Devish/transaction history"
 files = os.listdir(path)
 
 def selecting_right_file():
-    year = "2023"
-    # year = str(datetime.datetime.now().year)
+    year = str(datetime.datetime.now().year)
     for i, file in enumerate(files):
         if(year == file[20:24]):
             return(i)
@@ -18,24 +16,18 @@ def selecting_right_file():
     new_excel = pd.ExcelWriter(path + "/" + f"transaction_history({year}).xlsx", engine = 'xlsxwriter')
     intial_dataframe.to_excel(new_excel, sheet_name = "Sheet1", index = False)
     new_excel.save()
+    # new_excel.close()
     files.append(f"transaction_history({year}).xlsx")
-    return(len(files) - 1, new_excel)
+    return(len(files) - 1)
 
 
-def reading_excel(file_index):
-    # Reading the excel file besed on the file_index.
+def appending_excel(file_index):
     dataframe = pd.read_excel(path + '/' + files[file_index], engine= "openpyxl")
-    # print(dataframe)
-    return(dataframe)
+    input_dataframe = pd.DataFrame({"Date" : ["assa"], "Time" : ["1231"], "Amount of transaction" : [1235],	"Initial amount" : [45645], "Final Amount" : [65465], "Mode of transaction" : ["adad"], "Tansaction id" : ["sdads"], "Alert" : ["asdas"],	"Error: Reason" : ["adsda"]})
+    dataframe = pd.concat([dataframe, input_dataframe], ignore_index = True)
+    dataframe.to_excel(path + '/' + files[file_index], sheet_name='Sheet1', index = False)
+    
 
 
-def appending_excel(dataframe, excel):
-    dataframe.loc[len(dataframe.index)] = ["assa", "1231", 1235, 45645, 65465, "adad", "sdads","asdas", "adsda"] 
-    dataframe.to_excel(excel, index=False, header=False)
-    print(dataframe)
-
-
-i, excel = selecting_right_file()
-dataframe = reading_excel(i)
-appending_excel(dataframe, excel)
-excel.close()
+i = selecting_right_file()
+appending_excel(i)
