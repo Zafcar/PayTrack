bc6654cbf1f3a7c65a10220835ac7711d1d628c3
Zafcar <50046161+Zafcar@users.noreply.github.com>
2022-07-16 19:47:27 +0530

    Cashflow function
    Created a working cashflow function

--- pages/statistics_page.py
+++ pages/statistics_page.py
@@ -7,13 +7,19 @@ from transaction_statistics import *
 excel_files, excel_index = select_file()
 option = st.selectbox('Select an Excel file', excel_files)
 dataframe = reading_excel(excel_index[option])
-dataframe = dataframe.astype(str)
-st.dataframe(dataframe)
 
-# st.bar_chart(dataframe[:, [0, 2]])
+# Converting all the cloumns in the excel to string for displaying it.
+display_dataframe = dataframe.astype(str)
 
-chart_data = pd.DataFrame(
-     np.random.randn(50, 3),
-     columns=["a", "b", "c"])
+# Displaying the dataset of slected excel sheet.
+st.dataframe(display_dataframe)
 
-st.bar_chart(chart_data)
+# Displaying the amount spends.
+in_flow, out_flow = cashflow(dataframe)
+total_amount_spend, amount_added, amount_spend = st.columns(3)
+if(in_flow + out_flow > 0):
+     st.markdown(f'<p style="color:#33ff33;">↑ {str(round(in_flow + out_flow, 2))}</p>', unsafe_allow_html=True)
+else:
+     st.markdown(f'<p style="color:#FF5733;">↓ {str(round(in_flow + out_flow, 2))}</p>', unsafe_allow_html=True)
+st.markdown(f'<p style="color:#33ff33;">↑ {str(round(in_flow, 2))}</p>', unsafe_allow_html=True)
+st.markdown(f'<p style="color:#FF5733;">↓ {str(round(out_flow, 2))}</p>', unsafe_allow_html=True)
