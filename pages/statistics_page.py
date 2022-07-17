f3205a2eb1e08587a7a4740dbd5eccc81b7367b8
Zafcar <50046161+Zafcar@users.noreply.github.com>
2022-07-17 17:39:18 +0530

    Finished displaying amount.
    Completed the displaying of the cash flow with individual colour coding, need to add label to each of them.

--- pages/statistics_page.py
+++ pages/statistics_page.py
@@ -15,11 +15,14 @@ display_dataframe = dataframe.astype(str)
 st.dataframe(display_dataframe)
 
 # Displaying the amount spends.
+col1, col2, col3 = st.columns(3)
 in_flow, out_flow = cashflow(dataframe)
 total_amount_spend, amount_added, amount_spend = st.columns(3)
 if(in_flow + out_flow > 0):
-     st.markdown(f'<p style="color:#33ff33;">↑ {str(round(in_flow + out_flow, 2))}</p>', unsafe_allow_html=True)
+     col1.markdown(f'<h1 style="color:#33cc33;">↑ {str(round(in_flow + out_flow, 2))}</h1>', unsafe_allow_html=True)
 else:
-     st.markdown(f'<p style="color:#FF5733;">↓ {str(round(in_flow + out_flow, 2))}</p>', unsafe_allow_html=True)
-st.markdown(f'<p style="color:#33ff33;">↑ {str(round(in_flow, 2))}</p>', unsafe_allow_html=True)
-st.markdown(f'<p style="color:#FF5733;">↓ {str(round(out_flow, 2))}</p>', unsafe_allow_html=True)
+     col1.markdown(f'<h1 style="color:#ff0000;">↓ {str(round(in_flow + out_flow, 2))}</h1>', unsafe_allow_html=True)
+col2.markdown(f'<h1 style="color:#33cc33;">↑ {str(round(in_flow, 2))}</h1>', unsafe_allow_html=True)
+col3.markdown(f'<h1 style="color:#ff0000;">↓ {str(round(out_flow, 2))}</h1>', unsafe_allow_html=True)
+
+# Displaying graph based on date.
