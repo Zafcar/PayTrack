74a0143ec3908a0d8bc13afca72b22b92121eece
Zafcar <50046161+Zafcar@users.noreply.github.com>
2022-07-25 18:14:55 +0530

    Added comments
    Added comments to the methods and commented unfinished functions also added read_method function.

--- transaction_update.py
+++ transaction_update.py
@@ -3,15 +3,26 @@ import pandas as pd
 import os
 import datetime
 
-# Selecting which execl to read.
+# Path where the excel sheets exist.
 path = "C:/Devish/transaction history"
+# Returns a list of excel sheets present in the path.
 files = os.listdir(path)
 
+
+def reading_excel(file_index):
+    # Reading the excel file besed on the file_index.
+    dataframe = pd.read_excel(path + '/' + files[file_index], engine= "openpyxl")
+    # print(dataframe)
+    return(dataframe)
+
+
 def selecting_right_file():
+    #Opens the file based on the current year, if the year does not exist it will come out of the loop.
     year = str(datetime.datetime.now().year)
     for i, file in enumerate(files):
         if(year == file[20:24]):
             return(i)
+    #If the excel sheet of the current year is not present it will create a new excel sheet.
     intial_dataframe = pd.DataFrame({"Date" : [], "Time" : [], "Amount of transaction" : [], "Initial amount" : [], "Final Amount" : [], "Mode of transaction" : [], "Tansaction id" : [],"Alert" : [], "Error: Reason" : []})
     new_excel = pd.ExcelWriter(path + "/" + f"transaction_history({year}).xlsx", engine = 'xlsxwriter')
     intial_dataframe.to_excel(new_excel, sheet_name = "Sheet1", index = False)
@@ -21,8 +32,8 @@ def selecting_right_file():
     return(len(files) - 1)
 
 
-def appending_excel(file_index):
-    dataframe = pd.read_excel(path + '/' + files[file_index], engine= "openpyxl")
-    input_dataframe = pd.DataFrame({"Date" : ["assa"], "Time" : ["1231"], "Amount of transaction" : [1235],	"Initial amount" : [45645], "Final Amount" : [65465], "Mode of transaction" : ["adad"], "Tansaction id" : ["sdads"], "Alert" : ["asdas"],	"Error: Reason" : ["adsda"]})
-    dataframe = pd.concat([dataframe, input_dataframe], ignore_index = True)
-    dataframe.to_excel(path + '/' + files[file_index], sheet_name='Sheet1', index = False)
+# def appending_excel(file_index):
+#     dataframe = pd.read_excel(path + '/' + files[file_index], engine= "openpyxl")
+#     input_dataframe = pd.DataFrame({"Date" : ["assa"], "Time" : ["1231"], "Amount of transaction" : [1235],	"Initial amount" : [45645], "Final Amount" : [65465], "Mode of transaction" : ["adad"], "Tansaction id" : ["sdads"], "Alert" : ["asdas"],	"Error: Reason" : ["adsda"]})
+#     dataframe = pd.concat([dataframe, input_dataframe], ignore_index = True)
+#     dataframe.to_excel(path + '/' + files[file_index], sheet_name='Sheet1', index = False)
