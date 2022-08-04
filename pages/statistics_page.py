8dddd4aedd64e09f11ae40679a3449773ff28ad9
Zafcar <50046161+Zafcar@users.noreply.github.com>
2022-08-04 14:58:22 +0530

    Just fixed the display text in of cash flow.

--- pages/statistics_page.py
+++ pages/statistics_page.py
@@ -16,11 +16,11 @@ col1, col2, col3 = st.columns(3)
 in_flow, out_flow = cashflow(dataframe)
 total_amount_spend, amount_added, amount_spend = st.columns(3)
 if(in_flow + out_flow >= 0):
-     col1.markdown(f'<h1 style="color:#33cc33;"><span style = "font-size: 20px;">Total cash flow</span><br>↑ ₹{str(round(in_flow + out_flow, 2))}</h1>', unsafe_allow_html=True)
+     col1.markdown(f'<h2 style="color:#33cc33;"><span style = "font-size: 20px;">Total cash flow</span><br>↑ ₹{str(round(in_flow + out_flow, 2))}</h1>', unsafe_allow_html=True)
 else:
-     col1.markdown(f'<h1 style="color:#ff0000;"><span style = "font-size: 20px;">Total cash flow</span><br>↓ ₹{str(round(in_flow + out_flow, 2))}</h1>', unsafe_allow_html=True)
-col2.markdown(f'<h1 style="color:#33cc33;"><span style = "font-size: 20px;">Cash inflow</span><br>↑ ₹{str(round(in_flow, 2))}</h1>', unsafe_allow_html=True)
-col3.markdown(f'<h1 style="color:#ff0000;"><span style = "font-size: 20px;">Cash outflow</span><br>↓ ₹{str(round(out_flow, 2))}</h1>', unsafe_allow_html=True)
+     col1.markdown(f'<h2 style="color:#ff0000;"><span style = "font-size: 20px;">Total cash flow</span><br>↓ ₹{str(round(in_flow + out_flow, 2))}</h1>', unsafe_allow_html=True)
+col2.markdown(f'<h2 style="color:#33cc33;"><span style = "font-size: 20px;">Cash inflow</span><br>↑ ₹{str(round(in_flow, 2))}</h1>', unsafe_allow_html=True)
+col3.markdown(f'<h2 style="color:#ff0000;"><span style = "font-size: 20px;">Cash spent</span><br>↓ ₹{str(round(out_flow, 2))}</h1>', unsafe_allow_html=True)
 
 
 # Displaying graph based on date.
